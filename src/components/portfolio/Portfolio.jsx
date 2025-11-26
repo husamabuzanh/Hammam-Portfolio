bb4c3ebf539bcbf80fed872ffb48af511f3bafe6
Husam Abuzina <iquik@MacBook-Pro-2.local>
2025-11-26 22:40:33 +0200

    fix logo name inside code

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import Projects from "./Projects";
-import card1 from "../../assets/images/logos/PPU.jpg";
-import card2 from "../../assets/images/logos/Aljebrini.jpg";
-import card3 from "../../assets/images/logos/Trico.png"; // rename logo folder
+import card1 from "../../assets/images/Logos/PPU.jpg";
+import card2 from "../../assets/images/Logos/Aljebrini.jpg";
+import card3 from "../../assets/images/Logos/Trico.png"; // rename logo folder
 
 const projectData = [
   {
